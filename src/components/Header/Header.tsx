e390ab76d69cb751b55933af37183b8875426220
Federico Mieres <fnmieres@gmail.com>
2023-08-13 13:45:23 +0200

    feat: improve app icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,8 +15,22 @@ function Header({ isLoading = false }: HeaderProps) {
     <AppBar position="relative">
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         <Link component={RouterLink} to="/podcasts" sx={{ display: "flex" }}>
-          <PodcastsIcon sx={{ mr: 2 }} />
-          <Typography variant="h6" color="inherit" noWrap>
+          <PodcastsIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
             Podcaster
           </Typography>
         </Link>
