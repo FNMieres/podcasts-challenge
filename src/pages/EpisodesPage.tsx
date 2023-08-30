1bcf9bb3cf31568164954282730a4356c6601ca6
Federico Mieres <fnmieres@gmail.com>
2023-08-30 12:53:16 +0200

    feat: podcast episodes counter

--- src/pages/EpisodesPage.tsx
+++ src/pages/EpisodesPage.tsx
@@ -1,4 +1,7 @@
 import { useParams } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 import { usePodcastEpisodes } from "../features/podcasts/podcastsHooks";
 import EpisodesList from "../features/podcasts/components/EpisodesList";
 
@@ -6,7 +9,18 @@ function EpisodesPage() {
   const { podcastId } = useParams<{ podcastId: string }>();
   const { episodes } = usePodcastEpisodes({ podcastId: podcastId! });
 
-  return episodes && <EpisodesList data={episodes} />;
+  return (
+    episodes && (
+      <Grid container gap={4}>
+        <Paper sx={{ p: 2, width: "100%" }}>
+          <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+            Episodes: {episodes.length}
+          </Typography>
+        </Paper>
+        <EpisodesList data={episodes} />;
+      </Grid>
+    )
+  );
 }
 
 export default EpisodesPage;
